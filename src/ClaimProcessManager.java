22f45fbeb94c5855af2672672ab1631677748cce
hly-22 <s3979290@rmit.edu.vn>
2024-04-02 13:26:43 +0700

    Update ClaimProcessManager.java

--- src/ClaimProcessManager.java
+++ src/ClaimProcessManager.java
@@ -1,12 +1,12 @@
-import java.util.List;
-
 /**
  * @author <Ly Minh Hanh - s3979290>
  */
 
+import java.util.List;
+
 public interface ClaimProcessManager {
     public boolean add(Claim claim);
-    public void update(Claim claim);
+    public boolean update(Claim claim);
     public boolean delete(Claim claim);
     public Claim getOne(String fID);
     public List<Claim> getAll();
