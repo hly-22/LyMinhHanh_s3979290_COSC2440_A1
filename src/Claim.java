57d0d415fef688508e6989423a10c85e0130ffd5
hly-22 <s3979290@rmit.edu.vn>
2024-04-02 13:15:27 +0700

    Update Claim.java

--- src/Claim.java
+++ src/Claim.java
@@ -4,6 +4,7 @@
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Claim {
@@ -16,7 +17,73 @@ public class Claim {
     private BigDecimal claimAmount;
     private ClaimStatus status;
     private String receiverBankingInfo;
+    private static int nextFID = 1;
 
+    public Claim(Customer insuredPerson, InsuranceCard cardNumber, LocalDate examDate, BigDecimal claimAmount) {
+        this.fID = generateFID();
+        this.claimDate = LocalDate.now();
+        this.insuredPerson = insuredPerson;
+        this.cardNumber = cardNumber;
+        this.examDate = examDate;
+        this.documentList = new ArrayList<>();
+        this.claimAmount = claimAmount;
+        this.status = ClaimStatus.NEW;
+        this.receiverBankingInfo = null;
+    }
+
+    protected synchronized String generateFID() {
+        String FID = "f-" + String.format("%010d", nextFID);
+        nextFID++;
+        return FID;
+    }
+    public boolean addDocument(String documentName) {
+        String claimID = getFID();
+        String cardNumber = getCardNumber().getCardNumber();
+        String fullDocument = claimID + "_" + cardNumber + "_" + documentName + ".pdf";
+
+        return documentList.add(fullDocument);
+    }
+
+    public void setReceiverBankingInfo(String bank, String name, String number) {
+        String bankingInfo = bank + " - " + name + " - " + number;
+        this.receiverBankingInfo = bankingInfo;
+    }
+
+    public String getFID() {
+        return fID;
+    }
+
+    public LocalDate getClaimDate() {
+        return claimDate;
+    }
+
+    public Customer getInsuredPerson() {
+        return insuredPerson;
+    }
+
+    public InsuranceCard getCardNumber() {
+        return cardNumber;
+    }
+
+    public LocalDate getExamDate() {
+        return examDate;
+    }
+
+    public List<String> getDocumentList() {
+        return documentList;
+    }
+
+    public BigDecimal getClaimAmount() {
+        return claimAmount;
+    }
+
+    public ClaimStatus getStatus() {
+        return status;
+    }
+
+    public String getReceiverBankingInfo() {
+        return receiverBankingInfo;
+    }
 }
 
 enum ClaimStatus {
