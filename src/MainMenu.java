1f220b702ab6834310bb475489391c25f73a98f8
hly-22 <s3979290@rmit.edu.vn>
2024-04-04 17:18:30 +0700

    Update MainMenu.java

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,3 @@
-import javax.xml.crypto.Data;
 import java.util.Scanner;
 
 /**
@@ -15,50 +14,54 @@ public class MainMenu {
     public void displayMenu() {
         System.out.println("---Welcome to the system---");
         System.out.println("Please choose an option.");
+        System.out.println();
         System.out.println("1. Add customer");
         System.out.println("2. Delete customer");
         System.out.println("3. Add insurance card");
         System.out.println("4. Delete insurance card");
         System.out.println("5. Add claim");
-        System.out.println("6. Delete claim");
-        System.out.println("7. Update claim");
+        System.out.println("6. Update claim");
+        System.out.println("7. Delete claim");
         System.out.println("8. Read a claim");
         System.out.println("9. Read all claims");
         System.out.println("0. Exit");
 
         int response = Integer.parseInt(scanner.nextLine());
         switch (response) {
-            case 1:
-                systemManager.addCustomer();
-                break;
-            case 2:
-                System.out.println("Enter the customer cID you want to delete: ");
+            case 1 -> systemManager.addCustomer();
+            case 2 -> {
+                System.out.println("Enter the customer cID you want to delete (c-xxxxxxx): ");
                 String cID = scanner.nextLine();
                 systemManager.deleteCustomer(cID);
-                break;
-            case 3:
-                boolean a = systemManager.addInsuranceCard();
-                break;
-            case 4:
+            }
+            case 3 -> systemManager.addInsuranceCard();
+            case 4 -> {
                 System.out.println("Enter the insurance card number you want to delete: ");
                 String cardNumber = scanner.nextLine();
                 systemManager.deleteInsuranceCard(cardNumber);
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 9:
-                break;
-            case 0:
+            }
+            case 5 -> systemManager.addClaim();
+            case 6 -> {
+                System.out.println("Enter the claim fID you want to update (f-xxxxxxxxxx): ");
+                String updateFID = scanner.nextLine();
+                systemManager.updateClaim(updateFID);
+            }
+            case 7 -> {
+                System.out.println("Enter the claim fID you want to delete (f-xxxxxxxxxx): ");
+                String deleteFID = scanner.nextLine();
+                systemManager.deleteClaim(deleteFID);
+            }
+            case 8 -> {
+                System.out.println("Enter the claim fID you want to display: ");
+                String readFID = scanner.nextLine();
+                systemManager.getOneClaim(readFID);
+            }
+            case 9 -> systemManager.getAllClaims();
+            case 0 -> {
                 System.out.println("Exiting...");
                 System.exit(0);
-            default:
-                System.out.println("Invalid option. Please try again");
+            }
+            default -> System.out.println("Invalid option. Please try again");
         }
     }
 }
