da24193a422649a3158a66fa25f2c1bc0b9dd432
hly-22 <s3979290@rmit.edu.vn>
2024-04-06 00:58:44 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        MainMenu mainMenu = new MainMenu(new SystemManager(new CustomerViewText(), new InsuranceCardViewText(), new ClaimViewText()));
+        mainMenu.displayWelcomeMessage();
+        mainMenu.displayMenu();
     }
 }
